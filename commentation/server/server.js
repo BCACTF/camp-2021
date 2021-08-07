06e240ca40b7ac2a230c14f89be247db6e564e5e
yusufsallam64 <ysallam64@gmail.com>
2021-08-07 19:29:39 -0400

    added commentation web problem

---
+++ commentation/server/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const PORT = 54321;
+
+//Create 'app' - handler for http requests
+const app = express();
+app.use(express.urlencoded({extended: false}));
+
+
+//Middleware to handle http requests
+app.use(logger('dev'));
+
+//Middleware to handle static requests
+app.use(express.static(path.join(__dirname, "/public")));
+
+//Routes!
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+const server = http.createServer(app);
+server.listen(PORT);
+console.log(`HTTP server listening at http://localhost:${PORT}`);
