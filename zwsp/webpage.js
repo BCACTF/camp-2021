9f6e241df0bef9c8f4e9379c7c3340ad548ff1e6
krisharora <43799189+kna27@users.noreply.github.com>
2021-08-28 21:47:07 -0400

    Remove random newlines

--- zwsp/webpage.js
+++ zwsp/webpage.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const path = require("path");
-
 const app = express();
 
 app.get("/",function (request,response){
@@ -19,13 +17,8 @@ app.get("/",function (request,response){
             </body>
         </html>`;
 
-
     response.send(userthing);
-        
-
 });
-
-app.use(express.static(path.join(__dirname,"public")));
 
 app.listen(1234);
 console.log("Listening on http://localhost:1234");
